4ebd7cd969c93e4c71c5f8715c156b9a8bc3f0ec
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-06-13 23:41:23 +0600

    not found added

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,12 @@
 import React from "react";
+import "./NotFound.css";
 
 const NotFound = () => {
   return (
-    <div>
-      <h3>Page not found.</h3>
-      <p>error! error!</p>
+    <div className="not-found">
+      <h1>404</h1>
+      <h3>Page not found...</h3>
+      <p>Error has occured!</p>
     </div>
   );
 };
