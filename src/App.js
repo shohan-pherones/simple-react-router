5a28f1737b77694bba1e4ce560390ed6c4d52e65
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-06-13 23:32:55 +0600

    router ready

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import Home from "./components/Home/Home";
 import Friends from "./components/Friends/Friends";
 import About from "./components/About/About";
 import NotFound from "./components/NotFound/NotFound";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Friends />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/friends" element={<Friends />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
