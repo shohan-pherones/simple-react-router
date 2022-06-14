5f2e4101a0e11923eaaa74e7c2e1a3698db13b17
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-06-14 15:41:41 +0600

    dynamic data loaded

--- src/components/FriendDetail/FriendDetail.jsx
+++ src/components/FriendDetail/FriendDetail.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const FriendDetail = () => {
-  const params = useParams();
-  console.log(params);
+  const { friendId } = useParams();
+  const [friend, setFriend] = useState({});
+  useEffect(() => {
+    const url = `https://jsonplaceholder.typicode.com/users/${friendId}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setFriend(data));
+  }, []);
   return (
     <div>
-      <h1>Detail coming soon...</h1>
+      <h3>Friend detail of: {friendId}</h3>
+      <h1>{friend.name}</h1>
+      <h2>Phone: {friend.phone}</h2>
+      <p>Website: {friend.website}</p>
+      <p>Company: {friend.company?.name}</p>
     </div>
   );
 };
